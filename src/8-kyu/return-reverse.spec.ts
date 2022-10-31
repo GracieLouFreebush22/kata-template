05c948c864366c5461e7889c9dd6269d18cd9bee
grace k <grace.kuperman5472@gmail.com>
2022-10-31 10:09:52 +0000

    tests added

--- src/8-kyu/return-reverse.spec.ts
+++ src/8-kyu/return-reverse.spec.ts
@@ -5,3 +5,11 @@ import reverseList from "./return-reverse"
 test("given list return reverse of list", () => {
     expect(reverseList(["hi", "my", "name", "is"])).toEqual(["is", "name","my","hi"]);
   });
+
+  test("given list return reverse of list", () => {
+    expect(reverseList([0,1,2,3,4])).toEqual([4,3,2,1,0]);
+  });
+
+  test("given list return reverse of list", () => {
+    expect(reverseList([ [0,0], ["hi", "world"], [true, false]])).toEqual([[true,false], ["hi", "world"], [0,0]])
+  })
